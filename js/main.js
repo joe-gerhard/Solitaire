5bc4bfcca031ec98eb4a7401a7386616561ad2d8
Joe Gerhard <jlgerhard@gmail.com>
2019-08-04 18:44:14 -0500

    getClickDestination added, still working on click logic

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const suits = ['s', 'h', 'c', 'd'];
 const values = ['A', '02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K']
 /*----- app's state (variables) -----*/ 
 
-let deck, pile, draw, stacks, winner;
+let deck, pile, draw, stacks, aces, winner, clickedCard;
 
 /*----- cached element references -----*/ 
 
@@ -23,19 +23,21 @@ const boardEls = {
     stack7: document.getElementById('stack7')
 }
 /*----- event listeners -----*/ 
-document.querySelector('#resetButton').addEventListener('click', init);
-document.querySelector('#pile').addEventListener('click', drawCard);
+// document.querySelector('#resetButton').addEventListener('click', init);
+// document.querySelector('#pile').addEventListener('click', drawCard);
+document.querySelector('#gameBoard').addEventListener('click', handleClick);
 
 /*----- functions -----*/
 init();
 
 function init() {
-    reset();
     deck = [];
     pile = [];
     draw = [];
     stacks = [[],[],[],[],[],[],[]]
+    aces = [[],[],[],[]]
     winner = null;
+    clickedCard = null;
     // make deck
     makeDeck();
     // shuffle deck
@@ -48,7 +50,7 @@ function init() {
 }
 
 function render() {
-
+    clearAllDivs();
     // for each card on each pile, render them with the card back showing
     // if it's the last card in the pile, render it with it's face up
     stacks.forEach((stack, sIdx) => {
@@ -112,23 +114,91 @@ function dealCards() {
 function drawCard () {
     if(pile.length > 0) {
         draw.push(pile.pop());
-        boardEls.pile.removeChild(boardEls.pile.lastChild)
-        renderDraw();
+        // boardEls.pile.removeChild(boardEls.pile.lastChild)
+        render();
     } else {
         while(draw.length > 0) {
             pile.push(draw.pop())
         }
         render();
-        while(boardEls.draw.firstChild) {
-            boardEls.draw.removeChild(boardEls.draw.firstChild);
-        }
+        // while(boardEls.draw.firstChild) {
+        //     boardEls.draw.removeChild(boardEls.draw.firstChild);
+        // }
     }
 }
 
-function reset() {
+function clearAllDivs() {
     for(let boardEl in boardEls) {
         while(boardEls[boardEl].firstChild) {
             boardEls[boardEl].removeChild(boardEls[boardEl].firstChild);
         }
     }
+}
+
+function handleClick(evt) {
+    let clickDest = getClickDestination(evt.target);
+
+    if (clickDest.includes('stack')) {
+        handleStackClick();
+    } else if (clickDest.includes('ace')) {
+        handleAceClick();
+    } else if (clickDest === 'draw') {
+        handleDrawClick();
+    } else if (clickDest === 'pile') {
+        drawCard();
+    } else if (clickDest === 'resetButton') {
+        init();
+    }
+    // if (!clickedCard && isFaceUpCard(evt.target)) {
+    //     clickedCard = evt.target;
+    //     clickedCard.className += ' highlight';
+    //     let stackId = clickedCard.parentNode.id.replace('stack', '') -1;
+    //     console.log(stacks[stackId][stacks[stackId].length-1]);
+    // } else if (isFaceUpCard(evt.target)) {
+    //     // check if clicked card is playable on destCard
+    //     let destCard = evt.target;
+    //     console.log(destCard.parentNode);
+    //     // play clicked card on top of the destCard
+    //     destCard.parentNode.appendChild(clickedCard);
+    //     clickedCard.className = clickedCard.className.replace(' highlight', '');
+    //     clickedCard = null;
+    // } else if (isAcePile(evt.target)) {
+    //     console.log(`that's an ace pile`)
+    // }
+    
+    // clickedCard.parentNode.removeChild(clickedCard)
+    // clickedCard = null;
+}
+
+function isFaceUpCard(element) {
+    return (element.className.includes('card') && !(element.className.includes('back')) && !(element.className.includes('outline'))) 
+}
+
+function isAcePile(element) {
+    if (!(element.firstChild)) {
+        return element.id.includes('ace');
+    } else {
+        return element.parentNode.id.includes('ace');
+    }
+}
+
+function getClickDestination(element) {
+    if (element.id) {
+        return element.id;
+    } 
+    else {
+        return element.parentNode.id;
+    }
+}
+
+function handleStackClick() {
+    console.log('handling Stack click');
+}
+
+function handleAceClick() {
+    console.log('handling Ace click');
+}
+
+function handleDrawClick() {
+    console.log('handlingDrawClick')
 }

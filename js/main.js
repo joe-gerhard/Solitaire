416adbdf8b6f77bfa0188f8d3d7251badbcf4914
Joe Gerhard <jlgerhard@gmail.com>
2019-08-05 10:51:28 -0500

    reset to make sure all stack plays are legal

--- js/main.js
+++ js/main.js
@@ -228,7 +228,7 @@ function handleStackClick(element) {
             clickedCard = null;
             render();
         // check if play is legal
-        } else if(true || isPlayLegal(clickedCard, topCard)){
+        } else if(isPlayLegal(clickedCard, topCard)){
             while(cardArr.length > 0) {
                 stacks[stackId].push(cardArr.pop());
                 stacksFaceUp[stackId]++;
